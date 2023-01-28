08be5dfede849e402b234aacdef044da750bed3c
ShariqAnsari88 <36728316+ShariqAnsari88@users.noreply.github.com>
2023-01-29 04:19:46 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
-import PageNotFound from "./pages/404/pageNotFound";
+import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
     const dispatch = useDispatch();
--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 import "./style.scss";
 
-const pageNotFound = () => {
-    return <div>pageNotFound</div>;
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
 };
 
-export default pageNotFound;
+export default PageNotFound;
