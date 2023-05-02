8c17237114fe84d477c34ea2daa3cc3ba51e83ee
Sanjay Ganapathi <gana.sanjay12@gmail.com>
2023-05-02 22:01:32 +0530

    fixed tvshows date

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -92,7 +92,7 @@ const Carousel = ({ data, loading, endpoint, title }) => {
                                             {item.title || item.name}
                                         </span>
                                         <span className="date">
-                                            {dayjs(item.release_date).format(
+                                            {dayjs(item.release_date || item.first_air_date).format(
                                                 "MMM D, YYYY"
                                             )}
                                         </span>
